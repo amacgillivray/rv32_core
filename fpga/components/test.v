8d36eec32d41f9bf382f626103070f97c21e273c
Andrew MacGillivray <agjmacgill@gmail.com>
2022-10-02 12:02:05 -0500

    Improve ALU test Tested on jdoodle

--- fpga/components/test.v
+++ fpga/components/test.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps  
+`include "alu.v"
 // test the adder at https://www.jdoodle.com/execute-Verilog-online
+// copy and paste the contents of alu.v below the jdoodle module
+
 module jdoodle;
- reg[7:0] A;
- reg[7:0] B;
+ reg[31:0] A;
+ reg[31:0] B;
  reg[3:0] sel;
- wire[7:0] out;
+ wire[31:0] out;
  wire carry;
  integer i;
     alu test_unit(
@@ -15,8 +18,8 @@ module jdoodle;
             carry
     );
     initial begin
-      A = 8'b11010101;
-      B = 8'b00001010;
+      A = 32'hEF2598;
+      B = 32'hABC999;
       sel = 4'b0000;
 
       // Loop through all possible operations of the ALU
@@ -25,6 +28,42 @@ module jdoodle;
         // Show the current state of the inputs
         $display("Program Inputs:");
         $display("\tA: %d\n\tB: %d\n\tSel: %b", A, B, sel);
+        
+        // Indicate what operation is being performed
+        case (sel)
+            4'b0000: // Add
+                $display("Operation: +");
+            4'b0001: // Subtract
+                $display("Operation: -");
+            4'b0010: // Multiply
+                $display("Operation: *");
+            4'b0011: // Divide
+                $display("Operation: /");
+            4'b0100: // Shift Left
+                $display("Operation: <<");
+            4'b0101: // Shift Right
+                $display("Operation: >>");
+            4'b0110: // Rotate Left
+                $display("Operation: RoL");
+            4'b0111: // Rotate Right
+                $display("Operation: RoR"); 
+            4'b1000: // Logial AND 
+                $display("Operation: &");
+            4'b1001: // Logical OR
+                $display("Operation: |");
+            4'b1010: // Logical XOR
+                $display("Operation: ^");
+            4'b1011: // Logical NOR
+                $display("Operation: ~|");
+            4'b1100: // Logical NAND
+                $display("Operation: ~&");
+            4'b1101: // Logical XNOR
+                $display("Operation: ~^");
+            4'b1110: // Greater than
+                $display("Operation: > ?");
+            4'b1111: // Equal 
+                $display("Operation: == ?");
+        endcase
         // Fire the circuit
         #10;
         // Show the Resulting Ouputs
@@ -32,59 +71,9 @@ module jdoodle;
         $display ("\tValue: %d\n\tCarry: %b\n", out, carry);
         // Increment selector bits by 1 to go to the next
         // type of operation
-        sel = sel + 8'h01;
+        sel = sel + 4'h1;
       end;
       $finish;
     end
 endmodule
 
-module alu(
-    input [7:0] A,
-    input [7:0] B,
-    input [3:0] sel,
-    output [7:0] out,
-    output carry
-    );
-    reg[7:0] result;
-    wire[8:0] tmp;
-    assign out = result;
-    assign tmp = {1'b0,A} + {1'b0,B};
-    assign carry = tmp[8];
-    always @(*)
-    begin
-        case(sel)
-            4'b0000:
-                result = A + B;
-            4'b0001: 
-                result = A - B;
-            4'b0010:
-                result = A * B;
-            4'b0011: 
-                result = A / B;
-            4'b0100: 
-                result = A << 1;
-            4'b0101:
-                result = A >> 1;
-            4'b0110: 
-                result = {A[6:0],A[7]};
-            4'b0111: 
-                result = {A[0],A[7:1]}; 
-            4'b1000:
-                result = A & B;
-            4'b1001:
-                result = A | B;
-            4'b1010: 
-                result = A ^ B;
-            4'b1011:
-                result = ~(A | B);
-            4'b1100:
-                result = ~(A & B);
-            4'b1101:
-                result = ~(A ^ B);
-            4'b1110:
-                result = ~(A>B)?8'd1:8'd0;
-            4'b1111:
-                result = ~(A==B)?'d1:8'd0;
-            endcase
-        end
-endmodule
