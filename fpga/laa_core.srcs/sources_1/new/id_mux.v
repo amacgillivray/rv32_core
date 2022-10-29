8e6ec0b473f3bbe40ba42c39ea96a277eef21e2f
Jarrod <jarrodgro@gmail.com>
2022-10-29 16:06:58 -0500

    added wb mux for the wb stage and data memory for the mem stage.

---
+++ fpga/laa_core.srcs/sources_1/new/id_mux.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/28/2022 06:20:18 PM
+// Design Name: 
+// Module Name: id_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module id_mux(
+    input branch_address,
+    input jalr_address,
+    output jump_target
+    );
+endmodule
