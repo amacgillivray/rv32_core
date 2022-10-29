ba18dec0782204cb4e9f380f2f4f9c32748bffe7
Andrew MacGillivray <agjmacgill@gmail.com>
2022-10-29 17:31:17 -0500

    Add more components, update diagram

---
+++ fpga/laa_core.srcs/sources_1/new/latch_if-id.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Andrew MacGillivray 
+// 
+// Create Date: 10/29/2022 04:36:55 PM
+// Design Name: 
+// Module Name: latch_ifid
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: Latch to separate Instruction Fetch and Instruction Decode 
+// stages of the RISC-V 5-stage pipeline.
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module latch_ifid(
+    input clock,
+    input [31:0] ReadInstruction,
+    input [31:0] PC,
+    output [31:0] Latched_ReadInstruction,
+    output [31:0] Latched_PC
+    );
+    reg [31:0] ri_tmp; // read instruction
+    reg [31:0] pc_tmp; // program counter
+    assign Latched_ReadInstruction = ri_tmp; 
+    assign Latched_PC = pc_tmp;
+    always @(negedge clock) begin
+        ri_tmp = ReadInstruction;
+        pc_tmp = PC;
+    end
+endmodule
