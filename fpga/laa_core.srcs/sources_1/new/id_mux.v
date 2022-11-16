7e020be4391d607c20ed9a235fbe7499af56b9a0
Andal01 <45972120+Andal01@users.noreply.github.com>
2022-11-16 11:20:29 -0600

    Manual Merge from local

--- fpga/laa_core.srcs/sources_1/new/id_mux.v
+++ fpga/laa_core.srcs/sources_1/new/id_mux.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineers: Jarrod Grothusen, Andrew MacGillivray
+// Engineer: Jarrod Grothusen
 // 
 // Create Date: 10/28/2022 06:20:18 PM
 // Design Name: 
@@ -19,21 +19,10 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
 module id_mux(
-    input IsJalr,
-    input[31:0] branch_address,
-    input [31:0] jalr_address,
-    output [31:0] jump_target
+    input branch_address,
+    input jalr_address,
+    output jump_target
     );
-    reg [31:0] result;
-    assign jump_target = result;
-    always @(*)
-    begin
-        case (IsJalr)
-            1'b0: // is not JALR
-                result = branch_address;
-            1'b1: // is JALR 
-                result = jalr_address;
-        endcase
-    end
 endmodule
