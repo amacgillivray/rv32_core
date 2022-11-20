1b5eefa6e6212a2266f1fd595c612b6a177905aa
Andal01 <45972120+Andal01@users.noreply.github.com>
2022-11-20 15:41:25 -0600

    framework for alu control and forward units

---
+++ core/ex_ALU_cntrl.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Jarrod Grothusen
+// 
+// Create Date: 
+// Design Name: 
+// Module Name: ex_ALU_cntrl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ex_ALU_cntrl(
+    input ALU_OP,
+    input instr,
+    output result
+    );
+    
+endmodule
