4c32f9e9989723f5b7dfe20b9520a78424cdae14
Andrew <40438470+amacgillivray@users.noreply.github.com>
2023-03-20 16:24:45 -0500

    More fixes, additions

--- re/defs.v
+++ re/defs.v
@@ -43,9 +43,9 @@
 
 
 /* PRIVILEGE LEVELS */
-`define PRIV_MACHINE 2'd3;
-`define PRIV_SUPER 2'd1;
-`define PRIV_USER 2'd0;
+`define PRIV_MACHINE 2'd3
+`define PRIV_SUPER 2'd1
+`define PRIV_USER 2'd0
 
 /* ALU Operations -- defined in order they appear in alu.v */
 `define ALU_ADD                     4'b0100
@@ -68,10 +68,10 @@
    carried out.
  */
 // Mask all but Funct7, Funct3, and Opcode fields
-`define M_RTYPE = 32'hfe00707f
+`define M_RTYPE 32'hfe00707f
 
 // Mask all but Funct3 and Opcode fields
-`define M_ITYPE = 32'h707f
+`define M_ITYPE 32'h707f
 
 
 /* Instructions
@@ -291,7 +291,7 @@
 `define M_IFENCE `M_ITYPE
 
 /* TODO - Atomics */
-/* INSTRUCTIONS - RISCV M EXTENSION - Multiplication / Division */
+/* RISCV M EXTENSION INSTRUCTIONS -- Multiplication / Division */
 // MUL
 `define I_MUL 32'h2000033
 `define M_MUL `M_RTYPE
@@ -325,22 +325,116 @@
 `define M_REMU `M_RTYPE
 /* TODO - Vector Extension Instructions */
 
+
+//--------------------------------------------------------------------
+// CSR Registers
+//--------------------------------------------------------------------
+// taken directly from the other repository
+`define CSR_MSTATUS       12'h300
+`define CSR_MSTATUS_MASK  32'hFFFFFFFF
+`define CSR_MISA          12'h301
+`define CSR_MISA_MASK     32'hFFFFFFFF
+    `define MISA_RV32     32'h40000000
+    `define MISA_RVI      32'h00000100
+    `define MISA_RVE      32'h00000010
+    `define MISA_RVM      32'h00001000
+    `define MISA_RVA      32'h00000001
+    `define MISA_RVF      32'h00000020
+    `define MISA_RVD      32'h00000008
+    `define MISA_RVC      32'h00000004
+    `define MISA_RVS      32'h00040000
+    `define MISA_RVU      32'h00100000
+`define CSR_MEDELEG       12'h302
+`define CSR_MEDELEG_MASK  32'h0000FFFF
+`define CSR_MIDELEG       12'h303
+`define CSR_MIDELEG_MASK  32'h0000FFFF
+`define CSR_MIE           12'h304
+`define CSR_MIE_MASK      `IRQ_MASK
+`define CSR_MTVEC         12'h305
+`define CSR_MTVEC_MASK    32'hFFFFFFFF
+`define CSR_MSCRATCH      12'h340
+`define CSR_MSCRATCH_MASK 32'hFFFFFFFF
+`define CSR_MEPC          12'h341
+`define CSR_MEPC_MASK     32'hFFFFFFFF
+`define CSR_MCAUSE        12'h342
+`define CSR_MCAUSE_MASK   32'h8000000F
+`define CSR_MTVAL         12'h343
+`define CSR_MTVAL_MASK    32'hFFFFFFFF
+`define CSR_MIP           12'h344
+`define CSR_MIP_MASK      `IRQ_MASK
+`define CSR_MCYCLE        12'hc00
+`define CSR_MCYCLE_MASK   32'hFFFFFFFF
+`define CSR_MTIME         12'hc01
+`define CSR_MTIME_MASK    32'hFFFFFFFF
+`define CSR_MTIMEH        12'hc81
+`define CSR_MTIMEH_MASK   32'hFFFFFFFF
+`define CSR_MHARTID       12'hF14
+`define CSR_MHARTID_MASK  32'hFFFFFFFF
+// Non-std
+`define CSR_MTIMECMP        12'h7c0
+`define CSR_MTIMECMP_MASK   32'hFFFFFFFF
+
+
+//-----------------------------------------------------------------
+// CSR Registers - Supervisor
+//-----------------------------------------------------------------
+// Taken directly from the other repository
+`define CSR_SSTATUS       12'h100
+`define CSR_SSTATUS_MASK  `SR_SMODE_MASK
+`define CSR_SIE           12'h104
+`define CSR_SIE_MASK      ((1 << `IRQ_S_EXT) | (1 << `IRQ_S_TIMER) | (1 << `IRQ_S_SOFT))
+`define CSR_STVEC         12'h105
+`define CSR_STVEC_MASK    32'hFFFFFFFF
+`define CSR_SSCRATCH      12'h140
+`define CSR_SSCRATCH_MASK 32'hFFFFFFFF
+`define CSR_SEPC          12'h141
+`define CSR_SEPC_MASK     32'hFFFFFFFF
+`define CSR_SCAUSE        12'h142
+`define CSR_SCAUSE_MASK   32'h8000000F
+`define CSR_STVAL         12'h143
+`define CSR_STVAL_MASK    32'hFFFFFFFF
+`define CSR_SIP           12'h144
+`define CSR_SIP_MASK      ((1 << `IRQ_S_EXT) | (1 << `IRQ_S_TIMER) | (1 << `IRQ_S_SOFT))
+`define CSR_SATP          12'h180
+`define CSR_SATP_MASK     32'hFFFFFFFF
+
+
 /* STATUS REGISTER */
-`define SR_UIE (1 << 0)
-`define SR_UIE_R 0
-`define SR_SIE (1 << 1)
-`define SR_SIE_R 1
-`define SR_MIE (1 << 3)
-`define SR_MIE_R 3
-`define SR_UPIE (1 << 4)
+`define SR_UIE_R  0
+`define SR_UIE    (1 << `SR_UIE_R)
+`define SR_SIE_R  1
+`define SR_SIE    (1 << `SR_SIE_R)
+`define SR_MIE_R  3
+`define SR_MIE    (1 << `SR_MIE_R)
 `define SR_UPIE_R 4
-`define SR_SPIE (1 << 5)
+`define SR_UPIE   (1 << `SR_UPIE_R)
 `define SR_SPIE_R 5
-`define SR_MPIE (1 << 7)
+`define SR_SPIE   (1 << `SR_SPIE_R)
 `define SR_MPIE_R 7
-`define SR_SPP (1 << 8)
-`define SR_SPP_R 8
+`define SR_MPIE   (1 << `SR_MPIE_R)
+`define SR_SPP_R  8
+`define SR_SPP    (1 << `SR_SPP_R)
 `define SR_MPP_SHIFT 11
+`define SR_MPP_MASK  2'h3
+`define SR_MPP_R     12:11
+`define SR_MPP_U     `PRIV_USER
+`define SR_MPP_S     `PRIV_SUPER
+`define SR_MPP_M     `PRIV_MACHINE
+`define SR_SUM_R   18
+`define SR_SUM     (1 << `SR_SUM_R)
+`define SR_MPRV_R  17
+`define SR_MPRV    (1 << `SR_MPRV_R)
+`define SR_MXR_R   19
+`define SR_MXR     (1 << `SR_MXR_R)
+`define SR_SMODE_MASK (`SR_UIE | `SR_SIE | `SR_UPIE | `SR_SPIE | `SR_SPP | `SR_SUM)
+
+//--------------------------------------------------------------------
+// SATP definitions
+//--------------------------------------------------------------------
+// Taken directly from other repository
+`define SATP_PPN_R        19:0 // TODO: Should be 21?? (UE comment)
+`define SATP_ASID_R       30:22
+`define SATP_MODE_R       31
 
 /* Exception Constants */
 `define EXCEPTION_W                        6
