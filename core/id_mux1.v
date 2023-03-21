cf84f5066d0967e3ad759174f136dc6d32f6eb17
Andrew <40438470+amacgillivray@users.noreply.github.com>
2023-03-21 16:38:19 -0500

    Correct IDMUX1
    Fixes at least the syntax errors

--- core/id_mux1.v
+++ core/id_mux1.v
@@ -1,22 +1,14 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: EECS 581 Team 11
 // Engineers: Jarrod Grothusen, Andrew MacGillivray
 // 
 // Create Date: 10/28/2022 06:20:18 PM
-// Design Name: 
+// Design Name: Instruction Decode Mux 1
 // Module Name: id_mux
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
 // Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 module id_mux1(
@@ -29,23 +21,27 @@ module id_mux1(
     output [4:0] idex_m,
     output [5:0] idex_ex
     );
-    wire control;
+    reg control;
+    reg [1:0] wb;
+    reg [4:0] m;
+    reg [5:0] ex;
     always @(*)
     begin
-        if(stall || id_flush)
-            control = 1;
-        else
-            control = 0;        
-
+        control <= (stall | id_flush);
         case (control)
-            1'b0: // is not JALR
-                idex_wb = cntrl_wb;
-                idex_m = cntrl_m;
-                idex_ex = cntrl_ex;
-            1'b1: // is JALR 
-                idex_wb = 00;
-                idex_m = 00000;
-                idex_ex = 000000;
+            1'b0: begin // is not JALR
+                wb <= cntrl_wb;
+                m <= cntrl_m;
+                ex <= cntrl_ex;
+            end
+            1'b1: begin // is JALR 
+                wb <= 2'b00;
+                m <= 5'b00000;
+                ex <= 6'b000000;
+            end
         endcase
     end
+    assign idex_wb = wb;
+    assign idex_m  = m;
+    assign idex_ex = ex;
 endmodule
